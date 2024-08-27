daf1872e194f4420729917c7c1d3a6b8a5aca766
MarwanAl3dwan <marwanaladwan5@icloud.com>
2024-08-27 14:40:55 +0300

    Finalize SplashView and navigate to HomeView

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,15 +1,20 @@
 import 'package:go_router/go_router.dart';
 
+import '../../features/home/presentation/views/home_view.dart';
 import '../../features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
-  // static const String homeView = '/homeView';
+  static const String homeView = '/homeView';
 
   static final router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
         builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: homeView,
+        builder: (context, state) => const HomeView(),
       ),
     ],
   );
--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,14 +1,35 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: use_build_context_synchronously
 
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../core/utils/app_router.dart';
 import 'widgets/splash_view_body.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    _navigateToHomeView();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: SplashViewBody(),
     );
+  }
+
+  void _navigateToHomeView() {
+    Future.delayed(const Duration(seconds: 2), () {
+      GoRouter.of(context).pushReplacement(AppRouter.homeView);
+    });
   }
 }
