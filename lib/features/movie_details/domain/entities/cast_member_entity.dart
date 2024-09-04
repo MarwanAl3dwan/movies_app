de8f8c30ac6a957d0d666b4784d372d80926f7b3
MarwanAl3dwan <marwanaladwan5@icloud.com>
2024-09-04 21:20:50 +0300

    Create CastMember Model and Entity

---
+++ lib/features/movie_details/domain/entities/cast_member_entity.dart
@@ -0,0 +1,13 @@
+class CastMemberEntity {
+  final int memberId;
+  final String memberName;
+  final String memberProfilePath;
+  final String characterName;
+
+  CastMemberEntity({
+    required this.memberId,
+    required this.memberName,
+    required this.memberProfilePath,
+    required this.characterName,
+  });
+}
